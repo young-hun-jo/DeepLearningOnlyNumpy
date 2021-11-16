e73c664312b164b0680a268867a571e4e948662e
young-hun-jo <joyh951021@gmail.com>
2021-11-16 17:57:18 +0900

    feat: 기본활성화 함수, CEE, 수치미분 기능 추가

---
+++ common/functions.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def sigomid(x: np.array):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x: np.array):
+    return np.maximum(0, x)
+
+
+def softmax(x: np.array):
+    """
+    x: 2차원 데이터로 입력될 시, shape: (batch_size, feature)
+    """
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+    
+    x = x - np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
+
+
+def cross_entropy_error(y: np.array, t: np.array):
+    """
+    y: Softmax 확률 값
+    t: 클래스 정답
+    """
+    # 데이터가 1개 들어왔을 경우
+    if y.ndim == 1:
+        y = y.reshape(1, y.size)
+        t = t.reshape(1, t.size)
+    # 레이블을 One-hot -> label 형태로 변경
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+    
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
---
+++ common/gradients.py
@@ -0,0 +1,25 @@
+def numerical_gradient(f, x: np.array):
+    """
+    f: 손실함수
+    x: 입력 값
+    """
+    h = 1e-4
+    grad = np.zeros_like(x)
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        # f(x+h)
+        x[idx] = tmp_val + h
+        fx1 = f(x)
+        # f(x-h)
+        x[idx] = tmp_val - h
+        fx2 = f(x)
+        
+        grad[idx] = (fx1 - fx2) / (2*h)
+        
+        x[idx] = tmp_val
+        it.iternext()
+        
+    return grad
