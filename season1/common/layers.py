6045620a6cc36579c0cb9e780d823ca3e6b2839a
young-hun-jo <joyh951021@gmail.com>
2021-11-24 18:24:33 +0900

    feat: convolution, max pooling 추가

--- season1/common/layers.py
+++ season1/common/layers.py
@@ -1,4 +1,5 @@
 from common.functions import *
+from common.util import im2col, col2im
 import numpy as np
 
 #=============
@@ -106,3 +107,184 @@ class Dropout:
     
     def backward(self, dout):
         return dout * self.mask
+    
+# BatchNormalization clone
+class BatchNormalization:
+    """
+    http://arxiv.org/abs/1502.03167
+    """
+    def __init__(self, gamma, beta, momentum=0.9, running_mean=None, running_var=None):
+        self.gamma = gamma
+        self.beta = beta
+        self.momentum = momentum
+        self.input_shape = None # 합성곱 계층은 4차원, 완전연결 계층은 2차원  
+
+        # 시험할 때 사용할 평균과 분산
+        self.running_mean = running_mean
+        self.running_var = running_var  
+        
+        # backward 시에 사용할 중간 데이터
+        self.batch_size = None
+        self.xc = None
+        self.std = None
+        self.dgamma = None
+        self.dbeta = None
+
+    def forward(self, x, train_flg=True):
+        self.input_shape = x.shape
+        if x.ndim != 2:
+            N, C, H, W = x.shape
+            x = x.reshape(N, -1)
+
+        out = self.__forward(x, train_flg)
+        
+        return out.reshape(*self.input_shape)
+            
+    def __forward(self, x, train_flg):
+        if self.running_mean is None:
+            N, D = x.shape
+            self.running_mean = np.zeros(D)
+            self.running_var = np.zeros(D)
+                        
+        if train_flg:
+            mu = x.mean(axis=0)
+            xc = x - mu
+            var = np.mean(xc**2, axis=0)
+            std = np.sqrt(var + 10e-7)
+            xn = xc / std
+            
+            self.batch_size = x.shape[0]
+            self.xc = xc
+            self.xn = xn
+            self.std = std
+            self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * mu
+            self.running_var = self.momentum * self.running_var + (1-self.momentum) * var            
+        else:
+            xc = x - self.running_mean
+            xn = xc / ((np.sqrt(self.running_var + 10e-7)))
+            
+        out = self.gamma * xn + self.beta 
+        return out
+
+    def backward(self, dout):
+        if dout.ndim != 2:
+            N, C, H, W = dout.shape
+            dout = dout.reshape(N, -1)
+
+        dx = self.__backward(dout)
+
+        dx = dx.reshape(*self.input_shape)
+        return dx
+
+    def __backward(self, dout):
+        dbeta = dout.sum(axis=0)
+        dgamma = np.sum(self.xn * dout, axis=0)
+        dxn = self.gamma * dout
+        dxc = dxn / self.std
+        dstd = -np.sum((dxn * self.xc) / (self.std * self.std), axis=0)
+        dvar = 0.5 * dstd / self.std
+        dxc += (2.0 / self.batch_size) * self.xc * dvar
+        dmu = np.sum(dxc, axis=0)
+        dx = dxc - dmu / self.batch_size
+        
+        self.dgamma = dgamma
+        self.dbeta = dbeta
+        
+        return dx
+
+    
+# 컨볼루션 계층
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+        
+        # 역전파 시 시용할 데이터들
+        self.x = None
+        self.col = None
+        self.col_W = None
+        
+        # 역전파 시 계산할 파라미터 변화량
+        self.dW = None
+        self.db = None
+        
+        
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape  # 필터 shape
+        N, C, H, W = x.shape          # 입력 데이터 shape
+        out_h = 1 + int((H + 2*self.pad - FH) / self.stride)
+        out_w = 1 + int((W + 2*self.pad - FW))
+        
+        # 필터 사이즈를 고려한 입력 이미지 데이터 2차원 배열로 변환
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+        
+        out = np.dot(col, col_W) + self.b
+        out = out.reshape(N, out_h, out_w -1).transpose(0, 3, 1, 2)
+        
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+        
+        return out
+    
+    
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0,2,3,1).reshape(-1, FN)  # 역전파 계산 위해 2차원으로 변환
+        
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+        
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+        
+        return dx
+    
+    
+# Max Poooling 계층
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h  # 풀링 사이즈
+        self.pool_w = pool_w
+        self.stride = stride  # 풀링 윈도우 간격
+        self.pad = pad
+        
+        self.x = None
+        self.arg_max = None
+        
+        
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (H - self.pool_w) / self.stride)
+        
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+        
+        arg_max = np.argmax(col, axis=1)
+        # 2차원으로 flatten 시킨 후 열 방향으로 최댓값
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+        
+        self.x = x
+        self.arg_max = arg_max
+        
+        return out
+    
+    
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+        
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,))
+        
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+        
+        return dx
