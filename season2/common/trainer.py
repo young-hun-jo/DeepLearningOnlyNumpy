6ee7fcf4e8cdfc184f6fa5856b3290bc07221f40
young-hun-jo <joyh951021@gmail.com>
2021-12-14 19:43:29 +0900

    feat: RNNLM Trainer 클래스 추가

--- season2/common/trainer.py
+++ season2/common/trainer.py
@@ -74,9 +74,101 @@ class Trainer:
         plt.ylabel('손실')
         plt.show()
 
-                
 
+        
+# RNN Trainer 클래스
+class RnnlmTrainer:
+    """ T길이 시계열의 RNN 계층을 미니 배치 학습시키는 Trainer 클래스
+    
+    Args:
+        model: 생성한 RNN 모델
+        optimizer: 파라미터의 기울기 갱신하는 최적화 기법 ex) SGD, Adam, ...
+    
+    """
+    def __init__(self, model, optimizer):
+        self.model = model
+        self.optimizer = optimizer
+        self.time_idx = None
+        self.ppl_list = None
+        self.eval_interval = None
+        self.current_epoch = 0
+        
+        
+    def get_batch(self, x, t, batch_size, time_size):
+        """ 전체 시계열 데이터를 입력했을 때, 미니 배치 데이터 얻는 함수
+        
+        Args:
+            x: T길이의 시계열 입력 벡터
+            t: T길이의 시계열 정답
+            batch_size: 배치 사이즈
+            time_size: Truncated BPTT 수행 시, 작은 신경망 단위로 잘라낼 데이터 단위 수
+        
+        """
+        batch_x = np.empty((batch_size, time_size), dtype='i')
+        batch_t = np.empty((batch_size, time_size), dtype='i')
+        
+        data_size = len(x)
+        jump = data_size // batch_size
+        offsets = [i * jump for i in range(batch_size)]
+        
+        for time in range(time_size):  # 하나의 배치에서 time_size 길이만큼 데이터 할당 loop
+            for i, offset in enumerate(offsets):
+                batch_x[i, time] = x[(offset * self.time_idx) % data_size]
+                batch_t[i, time] = t[(offset * self.time_idx) % data_size]
+            self.time_idx += 1
+        return batch_x, batch_t
+    
+    
+    def fit(self, x, t, max_epoch=10, batch_size=20, time_size=35, max_grad=None, eval_interval=20):
+        data_size = len(x)
+        steps = data_size // (batch_size * time_size)
+        self.time_idx = 0
+        self.ppl_list = []
+        self.eval_interval = eval_interval
+        
+        model, optimizer = self.model, self.optimizer
+        total_loss = 0
+        loss_count = 0
+        
+        start_time = time.time()
+        for epoch in range(max_epoch):
+            for step in range(steps):
+                # 미니 배치 데이터 할당
+                batch_x, batch_t = self.get_batch(x, t, batch_size, time_size)
                 
+                # 순전파 및 역전파 수행 후 파라미터 갱신
+                loss = model.forward(batch_x, batch_t)
+                model.backward()
+                params, grads = remove_duplicate(model.params, model.grads)  # 중복 파라미터 제거
+                if max_grad is not None:
+                    clip_grads(grads, max_grad)  # gradient clipping(기울기 특정 값 넘어서면 자르기)
+                optimizer.update(params, grads)
+                total_loss += loss
+                loss_count += 1
+                
+                # perplexity 평가
+                if (eval_interval is not None) and (step % eval_interval) == 0:
+                    ppl = np.exp(total_loss / loss_count)
+                    runtime = time.time() - start_time
+                    print('|Epoch: %d|Step(출력 시 step): %d / %d|런타임 %d[s]|Perplexity: %.2f' 
+                          % (self.current_epoch+1, step+1, steps, runtime, ppl))
+                    self.ppl_list.append(float(ppl))
+                    total_loss, loss_count = 0, 0
+                    
+            self.current_epoch += 1
+                
+    
+    def plot(self, ylim=None):
+        x = np.arange(len(self.ppl_list))
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.plot(x, self.ppl_list, label='train')
+        plt.xlabel(f'측정 주기( x {self.eval_interval})')
+        plt.ylabel('Perplexity')
+        plt.show() 
+            
+            
+                         
 def remove_duplicate(params, grads):
     '''
     매개변수 배열 중 중복되는 가중치를 하나로 모아
