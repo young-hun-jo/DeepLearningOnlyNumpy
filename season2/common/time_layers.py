9f6ae0fa8d3d7a1ef5b8b0b728c928c1e985515c
young-hun-jo <joyh951021@gmail.com>
2021-12-20 14:55:00 +0900

    feat: 단일 LSTM, 전체 시계열 처리하는 TimeLSTM 계층 추가

--- season2/common/time_layers.py
+++ season2/common/time_layers.py
@@ -291,3 +291,184 @@ class TimeSoftmaxWithLoss:
         dx = dx.reshape((N, T, V))
         
         return dx
+    
+    
+    
+# 총 4개의 게이트의 각 Wx, Wh, b 파라미터를 결합해 Affine 변환으로 한 번에 계산!
+class LSTM:
+    """ 단일 LSTM 계층 클래스 구현
+    
+    Args:
+        Wx: 4개의 게이트에서 각각 입력 벡터 Xt 에 곱해지는 파라미터 Wx 4개
+        Wh: 4개의 게이트에서 각각 이전 은닉 상태 벡터 h_t-1 에 곱해지는 파라미터 Wh 4개
+        b: 4개의 게이트에서 각각 더해지는 편향 b 파라미터 4개
+    
+    """
+    def __init__(self, Wx, Wh, b):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.cache = None
+        
+        
+    def forward(self, x, h_prev, c_prev):
+        Wx, Wh, b = self.params
+        N, H = h_prev.shape  # 은닉 상태 벡터 차원 수 (batch_size, 노드 수)
+        
+        # 총 4개의 게이트에서의 아핀 변환을 한 번에 계산
+        A = np.matmul(x, Wx) + np.matmul(h_prev, Wh) + b
+        
+        # slicing 해서 각 게이트에 보내기
+        f = A[:, :H]
+        g = A[:, H:2*H]
+        i = A[:, 2*H:3*H]
+        o = A[:, 3*H:]
+        
+        f = sigmoid(f)
+        g = np.tanh(g)
+        i = sigmoid(i)
+        o = sigmoid(o)
+        
+        c_next = f * c_prev + g * i
+        h_next = np.tanh(c_next) * o
+        
+        self.cache = (x, h_prev, c_prev, f, g, i, o, c_next) # 역전파 시 사용할 데이터들 캐싱해두기
+        return h_next, c_next
+    
+    
+    def backward(self, dh_next, dc_next):
+        Wx, Wh, b = self.params
+        x, h_prev, c_prev, f, g, i, o, c_next = self.cache
+        #===============
+        # 게이트 역전파 수행
+        #===============
+        tanh_c_next = np.tanh(c_next)
+        
+        ds = dh_next * o * (1 - tanh_c_next**2) + dc_next
+        
+        dc_prev = ds * f  # 이전 기억 셀의 기울기
+        
+        # output 게이트
+        do = dh_next * tanh_c_next
+        do *= o * (1 - o)
+        # input 게이트
+        di = ds * g
+        di *= i * (1 - i)
+        # 새로운 기억 셀(main 게이트)
+        dg = ds * i
+        dg *= (1 - g**2)
+        # forget 게이트
+        df = ds * c_prev
+        df *= f * (1 - f)
+        
+        # 4개 게이트 기울기 가로로 결합, horizontal stack
+        dA = np.hstack((df, dg, di, do))
+        
+        #=================================
+        # Affine 변환(행렬 곱)에 대한 역전파 수행
+        #=================================
+        # 파라미터 기울기 계산
+        dWx = np.matmul(x.T, dA)
+        dWh = np.matmul(h_prev.T, dA)
+        db = dA.sum(axis=0)
+        
+        self.grads[0][...] = dWx
+        self.grads[1][...] = dWh
+        self.grads[2][...] = db
+        
+        # 입력, 은닉상태 벡터 기울기 계싼
+        dx = np.matmul(dA, Wx.T)
+        dh_prev = np.matmul(dA, Wh.T)
+        
+        return dx, dh_prev, dc_prev
+    
+    
+    
+# T 길이의 시계열 데이터를 한번에 처리하는 LSTM 계층
+class TimeLSTM:
+    """ 단일 LSTM 계층이 T개 있는 TimeLSTM 계층 구현
+    
+    Args:
+        Wx: 입력 벡터 x에 곱해지는 파라미터
+        Wh: 이전 LSTM 계층으로부터 흘러들어오는 은닉 상태 벡터 h에 곱해지는 파라미터
+        b: 편향 파라미터
+        stateful: 은닉 상태를 유지할지 여부. 유지하면 Truncated BPTT 수행 시 순전파를 끊지 않고 전달
+        
+    """
+    def __init__(self, Wx, Wh, b, stateful=False):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.layers = []
+        
+        self.h, self.c = None, None   # 은닉상태벡터, 기억셀
+        self.dh = None                # 이전 블록의 은닉 상태 기울기 값(이는 Truncated BPTT에서는 필요없지만 seq2seq에서 필요하기 때문이라고 함)
+        self.stateful = stateful
+    
+    def forward(self, xs):
+        """ xs라는 T 길이의 시계열 전체 입력 벡터를 순전파 수행
+        
+        Args:
+            xs: T 길이의 시곙려 전체 입력 벡터
+            
+        """
+        Wx, Wh, b = self.params
+        N, T, D = xs.shape       # (batch_size, time length, 입력벡터 차원 수)
+        H = Wh.shape[0]          # 은닉상태 벡터 차원 수
+        
+        self.layers = []
+        hs = np.empty((N, T, H), dtype='f')  # T 길이의 LSTM 계층 전체의 은닉 상태 벡터를 담을 배열 초기화
+        
+        if not self.stateful or self.h is None:
+            self.h = np.zeros((N, H), dtype='f')
+        if not self.stateful or self.c is None:
+            self.c = np.zeros((N, H), dtype='f')
+            
+        for t in range(T):
+            layer = LSTM(*self.params)
+            self.h, self.c = layer.forward(xs[:, t, :], self.h, self.c)  # loop다 돌면 마지막 은닉상태,기억셀이 들어있음!
+            hs[:, t, :] = self.h
+            
+            self.layers.append(layer)
+            
+        return hs
+    
+    
+    def backward(self, dhs):
+        """ T길이의 시계열 전체를 한 번에 Truncated BPTT 수행
+        
+        Args:
+            dhs: T길이 시계열 내의 모든 은닉 상태 벡터의 기울기
+        
+        """
+        Wx, Wh, b = self.params
+        N, T, H = dhs.shape
+        D = Wx.shape[0]  # 입력 벡터 차원 수
+        
+        dxs = np.empty((N, T, D), dtype='f') # T길이 시계열 내의 모든 입력 벡터의 기울기
+        dh, dc = 0, 0
+        
+        grads = [0, 0, 0] # 하나의 LSTM 계층에서 Wx, Wh, b 파라미터의 기울기 담을 리스트
+        for t in reversed(range(T)):
+            layer = self.layers[t]
+            dx, dh, dc = layer.backward(dhs[:, t, :] + dh, dc)  # dh는 분기의 역전파므로 sum
+            dxs[:, t, :] = dx
+            
+            # 하나의 LSTM 계층에서 Wx, Wh, b 파라미터의 기울기 담기
+            for i, grad in enumerate(layer.grads):
+                grads[i] += grad
+        
+        
+        # T길이 시계열 전체의 LSTM 계층에 걸쳐서 만들어진 Wx, Wh, b 파라미터 기울기 담기
+        for i, grad in enumerate(grads):
+            self.grads[i][...] = grad
+            
+        self.dh = dh  # 역전파 방향으로 보았을 때의 마지막 은닉 상태 벡터 기울기 값 캐싱 for seq2seq
+        
+        return dxs
+    
+    # 은닉상태, 기억셀 설정 함수
+    def set_state(self, h, c=None):
+        self.h, self.c = h, c
+    
+    # 은닉상태, 기억셀 초기화하여 순전파 시에도 유지 끊어버리기
+    def reset_state(self):
+        self.h, self.c = None, None
