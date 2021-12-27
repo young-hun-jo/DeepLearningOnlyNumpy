1c86a08b93eb7d27e95e8aa78d5907d993c0f158
young-hun-jo <joyh951021@gmail.com>
2021-12-27 16:32:56 +0900

    feat:간단한 덧셈 텍스트 데이터 seq2seq 학습

---
+++ season2/train/train_seq2seq_simple.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.append('..')
+import numpy as np
+import matplotlib.pyplot as plt
+
+from dataset import sequence
+from common.optimizer import Adam
+from common.trainer import Trainer
+from common.util import eval_seq2seq
+from seq2seq import Seq2Seq
+
+# 데이터 로드
+(x_train, t_train), (x_test, t_test) = sequence.load_data('addition.txt', seed=1984)
+char_to_id, id_to_char = sequence.get_vocab()
+
+# 하이퍼파라미터
+vocab_size = len(char_to_id)
+wordvec_size = 16
+hidden_size = 128
+batch_size = 128
+max_epoch = 25
+max_grad = 5.0
+
+# model, optimizer, Trainer 클래스
+model = Seq2Seq(vocab_size, wordvec_size, hidden_size)
+optimizer = Adam()
+trainer = Trainer(model, optimizer)
+
+acc_list = []
+for epoch in range(max_epoch):
+    trainer.fit(x_train, t_train, max_epochs=1, batch_size=batch_size, max_grad=max_grad)
+    
+    correct_num = 0
+    for i in range(len(x_test)):
+        question, correct = x_test[[i]], t_test[[i]]  # 1d-array로 반환
+        verbose = i < 10
+        correct_num += eval_seq2seq(model, question, correct, id_to_char, verbose)
+    
+    acc = float(correct_num) / len(x_test)
+    acc_list.append(acc)
+    print('검증 정확도: %.3f%%' % (acc * 100))
