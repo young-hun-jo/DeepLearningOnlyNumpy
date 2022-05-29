a32aba9ae6a64b053d3dae6a12f4541b43e02959
young-hun-jo <joyh951021@gmail.com>
2022-05-29 21:33:56 +0900

    지금까지 한 코드 복습

---
+++ season3/hands_on/practice1/dezero/__init__.py
@@ -0,0 +1,6 @@
+from dezero.core_simple import Variable, Function
+from dezero.core_simple import no_grad, using_config
+from dezero.core_simple import as_array, as_variable
+from dezero.core_simple import setup_variable
+
+setup_variable()
