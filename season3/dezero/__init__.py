02c1ef006f9553a510630a86336d67375795fdb9
young-hun-jo <joyh951021@gmail.com>
2022-07-03 16:12:01 +0900

    Step32까지 실습완료

--- season3/dezero/__init__.py
+++ season3/dezero/__init__.py
@@ -1,7 +1,15 @@
-from dezero.core_simple import Variable, Function
-from dezero.core_simple import using_config, no_grad
-from dezero.core_simple import as_array, as_variable
-from dezero.core_simple import setup_variable
+is_simple_core = False
 
+if is_simple_core:
+    from dezero.core_simple import Variable, Function
+    from dezero.core_simple import using_config, no_grad
+    from dezero.core_simple import as_array, as_variable
+    from dezero.core_simple import setup_variable
+
+else:
+    from dezero.core import Variable, Function
+    from dezero.core import using_config, no_grad
+    from dezero.core import as_array, as_variable
+    from dezero.core import setup_variable
 
 setup_variable()
