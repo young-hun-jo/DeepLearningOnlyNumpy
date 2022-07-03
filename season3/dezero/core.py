90e4801d476f79736428bc1be00fc0379257c4d0
young-hun-jo <joyh951021@gmail.com>
2022-07-03 16:11:50 +0900

    Step32까지 실습완료

---
+++ season3/dezero/core.py
@@ -0,0 +1,263 @@
+import numpy as np
+import heapq
+import weakref
+import contextlib
+
+
+class Config:
+    enable_backprop = True
+
+
+class Variable:
+    __array_priority__ = 200
+
+    def __init__(self, data, name=None):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f"{type(data)} dtype is not supported.")
+
+        self.data = data
+        self.name = name
+        self.grad = None
+        self.creator = None
+        self.generation = 0
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def clear_grad(self):
+        self.grad = None
+
+    def backward(self, use_heap=False, retain_grad=False, create_graph=False):
+        if self.grad is None:
+            self.grad = Variable(np.ones_like(self.data))
+
+        funcs = []
+        seen_sets = set()
+        flag = 0
+
+        def add_func(f):
+            if f not in seen_sets:
+                if not use_heap:
+                    funcs.append(f)
+                    seen_sets.add(f)
+                    funcs.sort(key=lambda x: x.generation)
+                else:
+                    heapq.heappush(funcs, (-f.generation, flag, f))
+                    seen_sets.add(f)
+        add_func(self.creator)
+
+        while funcs:
+            if not use_heap:
+                f = funcs.pop()
+            else:
+                f = heapq.heappop(funcs)[2]
+            gys = [y().grad for y in f.outputs]
+
+            with using_config('enable_backprop', create_graph):
+                gxs = f.backward(*gys)
+                if not isinstance(gxs, tuple):
+                    gxs = (gxs,)
+                for x, gx in zip(f.inputs, gxs):
+                    if x.grad is None:
+                        x.grad = gx
+                    else:
+                        x.grad = x.grad + gx
+
+                    if x.creator is not None:
+                        add_func(x.creator)
+                        flag += 1
+                flag = 0
+
+                # 중간 출력값들의 기울기 삭제
+                if not retain_grad:
+                    for y in f.outputs:
+                        y().grad = None
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p + ')'
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+
+class Function:
+    def __call__(self, *inputs):
+        inputs = [as_variable(x) for x in inputs]     # nd-array만 들어올 경우, Variable 클래스로 변환
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        # 역전파 활성모드
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError("This method should be run outside of Function class.")
+
+    def backward(self, gy):
+        raise NotImplementedError("This method should be run outside of Function class.")
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        return x0 + x1
+
+    def backward(self, gy):
+        return gy, gy
+
+
+class Mul(Function):
+    def forward(self, x0, x1):
+        return x0 * x1
+
+    def backward(self, gy):
+        x0, x1 = self.inputs
+        return x1 * gy, x0 * gy
+
+
+class Neg(Function):
+    def forward(self, x):
+        return -x
+
+    def backward(self, gy):
+        return -gy
+
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        return x0 - x1
+
+    def backward(self, gy):
+        return gy, -gy
+
+
+class Div(Function):
+    def forward(self, x0, x1):
+        return x0 / x1
+
+    def backward(self, gy):
+        x0, x1 = self.inputs
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+
+
+class Pow(Function):
+    def __init__(self, power):
+        self.power = power
+
+    def forward(self, x):
+        return x ** self.power
+
+    def backward(self, gy):
+        c = self.power
+        x, = self.inputs
+        gx = c * (x ** (c - 1)) * gy
+        return gx
+
+
+def add(x0, x1):
+    x1 = as_array(x1)
+    return Add()(x0, x1)
+
+
+def mul(x0, x1):
+    x1 = as_array(x1)
+    return Mul()(x0, x1)
+
+
+def neg(x):
+    return Neg()(x)
+
+
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)
+
+
+def pow(x, power):
+    return Pow(power)(x)
+
+
+@contextlib.contextmanager
+def using_config(name: str, value: bool):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+
+def no_grad():
+    return using_config('enable_backprop', False)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
+
+
+# 오버라이딩 적용 함수
+def setup_variable():
+    Variable.__add__ = add
+    Variable.__radd__ = add
+    Variable.__mul__ = mul
+    Variable.__rmul__ = mul
+    Variable.__neg__ = neg
+    Variable.__sub__ = sub
+    Variable.__rsub__ = rsub
+    Variable.__truediv__ = div
+    Variable.__rtruediv__ = rdiv
+    Variable.__pow__ = pow
