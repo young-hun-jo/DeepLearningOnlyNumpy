cce597cb44421497470c89b1c329f8ac3cd5b52d
young-hun-jo <joyh951021@gmail.com>
2022-08-13 16:44:18 +0900

    debug: 오류원인 찾음

--- season3/dezero/layers.py
+++ season3/dezero/layers.py
@@ -11,7 +11,6 @@ class Layer:
     def __setattr__(self, name, value):
         if isinstance(value, (Parameter, Layer)):
             self._params.add(name)
-        # avoid infinite callable
         super().__setattr__(name, value)
 
     def __call__(self, *inputs):
@@ -22,13 +21,12 @@ class Layer:
         self.outputs = [weakref.ref(y) for y in outputs]
         return outputs if len(outputs) > 1 else outputs[0]
 
-    def forward(self):
-        raise NotImplementedError(f"This method should be run outside of Layer class.")
+    def forward(self, x):
+        raise NotImplementedError("This method should be run outside of Layer class.")
 
     def params(self):
         for name in self._params:
             obj = self.__dict__[name]
-
             if isinstance(obj, Layer):
                 yield from obj.params()
             else:
@@ -53,17 +51,19 @@ class Linear(Layer):
         if nobias:
             self.b = None
         else:
-            self.b = Parameter(np.zeros(self.out_size, dtype=self.dtype), name='b')
+            self.b = Parameter(np.zeros(self.out_size).astype(self.dtype), name='b')
 
     def _init_W(self):
         I, O = self.in_size, self.out_size
-        W_data = np.random.randn(I, O).astype(self.dtype) * np.sqrt(1 / I)
+        W_data = np.random.rand(I, O).astype(self.dtype) * np.sqrt(1 / I)
         self.W.data = W_data
 
     def forward(self, x):
         if self.W.data is None:
             self.in_size = x.shape[1]
             self._init_W()
-        y = F.linear(x, self.W, self.b)   # Function의 call 매직 메소드 안에서 np.array로 변환후 계산 알아서 해줌
+        y = F.linear(x, self.W, self.b)
         return y
+
+
 
--- season3/test.py
+++ season3/test.py
@@ -4,8 +4,9 @@
 #     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import numpy as np
-from dezero import layers as L
+import dezero.layers as L
 from dezero import functions as F
+from dezero import Layer
 
 
 # dataset
@@ -13,40 +14,43 @@ np.random.seed(42)
 x = np.random.rand(100, 1)
 y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)   # label
 
-layer1 = L.Linear(out_size=10)
-layer2 = L.Linear(out_size=1)
+# build model
+model = Layer()
+model.l1 = L.Linear(10)
+model.l2 = L.Linear(1)
 
 
 def predict(x):
-    y = layer1(x)
-    z = F.sigmoid(y)
-    k = layer2(z)
-    return k
+    y = model.l1(x)
+    y = F.sigmoid(y)
+    y = model.l2(y)
+    return y
 
 
 lr = 0.2
 iters = 10000
 
 for i in range(iters):
-    # predict
+    # predict and get loss
     y_pred = predict(x)
-    # loss
     loss = F.mean_squared_error(y, y_pred)
 
-    # init gradients in parameters in middle layers for avoiding accumulated gradients(wrong gradients)
-    layer1.clear_grads()
-    layer2.clear_grads()
+    # clear grads
+    model.clear_grads()
 
-    # backward based on Loss function
+    # backward
     loss.backward(use_heap=True)
 
     # update parameters
-    for layer in [layer1, layer2]:
-        for param in layer.params():
-            param.data -= lr * param.grad.data
+    for param in model.params():
+        param.data -= lr * param.grad.data
 
-    if (i+1) % 1000 == 0:
-        print(f'Epoch:{i+1} -> Loss:', loss)
+    if i == 0 or (i+1) % 1000 == 0:
+        print(f"# Epoch:{i+1} -> Loss: {loss}")
+
+
+
+
 
 
 
