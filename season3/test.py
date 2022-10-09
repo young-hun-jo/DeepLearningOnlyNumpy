9e275d22f836af922c575eee9086829d7bb8adc8
young-hun-jo <joyh951021@gmail.com>
2022-10-09 17:33:21 +0900

    test: VGG16 로컬 테스트 구현

--- season3/test.py
+++ season3/test.py
@@ -3,75 +3,31 @@
 #     import os, sys
 #     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-import sys
-import logging
+import numpy as np
 import dezero
-import dezero.functions as F
-from dezero import DataLoader
-from dezero.models import MLP
-from dezero.optimizers import AdaGrad
-from dezero.datasets import Spiral
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logging.basicConfig(stream=sys.stdout,
-                    format='[%(asctime)s] %(levelname)s : %(message)s')
+from PIL import Image
+from dezero.models import VGG16
+from dezero.datasets import ImageNet
 
-# hyper-parameter
-max_epoch = 300
-batch_size = 30
-hidden_size = (10, 5)
-lr = 1.0
+# load input image
+url = 'https://github.com/WegraLee/deep-learning-from-scratch-3/blob/images/zebra.jpg?raw=true'
+img_path = dezero.utils.get_file(url)
+img = Image.open(img_path)
 
-# dataset
-train_set = Spiral(train=True)
-test_set = Spiral(train=False)
+x = VGG16.preprocess(img)
+x = x[np.newaxis]
 
-# dataloader
-train_loader = DataLoader(train_set, batch_size, shuffle=True)
-test_loader = DataLoader(test_set, batch_size, shuffle=False)
+# model
+model= VGG16(pretrained=True)
 
-# build and compile model
-model = MLP((*hidden_size, 3))
-optimizer = AdaGrad(lr=lr).setup(model)
+with dezero.test_mode():
+    y = model(x)
+pred_id = np.argmax(y.data)
 
-# train and test(validation) in each Epoch
-for epoch in range(max_epoch):
-    sum_loss, sum_acc = 0, 0
-
-    # mini-batch for train
-    for x, t in train_loader:
-        # predict and get loss, accuracy
-        y_pred = model(x)
-        loss = F.softmax_cross_entropy(y_pred, t)
-        acc = F.accuracy(y_pred, t)
-
-        # clear gradients
-        model.clear_grads()
-        # backward
-        loss.backward(use_heap=True)
-        # update params
-        optimizer.update()
-
-        # verbose metric
-        sum_loss += float(loss.data) * len(t)
-        sum_acc += float(acc.data) * len(t)
-    if (epoch+1) % 20 == 0:
-        logging.info(f"(Epoch{epoch+1})Train Loss: {sum_loss / len(train_set)}, Accuracy: {sum_acc / len(train_set)}")
-
-    # mini-batch for test
-    sum_loss, sum_acc = 0, 0
-    with dezero.no_grad():   # deactivate backward
-        for x, t in test_loader:
-            y_pred = model(x)
-            loss = F.softmax_cross_entropy(y_pred, t)
-            acc = F.accuracy(y_pred, t)
-            sum_loss += float(loss.data) * len(t)
-            sum_acc += float(acc.data) * len(t)
-    if (epoch+1) % 20 == 0:
-        logging.info(f"(Epoch{epoch+1})Test Loss: {sum_loss / len(test_set)}, Accuracy: {sum_acc / len(test_set)}")
-        print()
-
+model.plot(x, to_file='vgg.pdf')
+labels = ImageNet.labels()
+print('## Prediction result:', labels[pred_id])
 
 
 
