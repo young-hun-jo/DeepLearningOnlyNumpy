a20eaaa54496449df0dbd439216e22f82289a585
young-hun-jo <joyh951021@gmail.com>
2022-10-10 15:13:23 +0900

    test: Sin 더미 데이터 생성

--- season3/dezero/datasets.py
+++ season3/dezero/datasets.py
@@ -84,3 +84,20 @@ class ImageNet(Dataset):
         with open(path, 'r') as f:
             labels = eval(f.read())
         return labels
+
+
+class SinCurve(Dataset):
+    def prepare(self):
+        num_data = 1000
+        dtype = np.float64
+
+        x = np.linspace(0, 2 * np.pi, num_data)
+        noise_range = (-0.05, 0.05)
+        noise = np.random.uniform(noise_range[0], noise_range[1], size=x.shape)
+        if self.train:
+            y = np.sin(x) + noise
+        else:
+            y = np.cos(x)
+        y = y.astype(dtype)
+        self.data = y[:-1][:, np.newaxis]
+        self.label = y[1:][:, np.newaxis]
