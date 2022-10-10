e02d8c50cf33d236c10cdf77f3232bd1f2014fc4
young-hun-jo <joyh951021@gmail.com>
2022-10-10 14:28:24 +0900

    feat: RNN BPTT 구현

--- season3/dezero/core.py
+++ season3/dezero/core.py
@@ -101,6 +101,19 @@ class Variable:
     def sum(self, axis=None, keepdims=False):
         return dezero.functions.sum(self, axis, keepdims)
 
+    def unchain(self):
+        self.creator = None
+
+    def unchain_backward(self):
+        if self.creator is not None:
+            funcs = [self.creator]
+            while funcs:
+                f = funcs.pop()
+                for x in f.inputs:
+                    if x.creator is not None:
+                        funcs.append(x.creator)
+                        x.unchain()
+
     @property
     def T(self):
         return dezero.functions.transpose(self)
